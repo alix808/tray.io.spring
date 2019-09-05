d313374ab0b490fe8d7ea22e0eed7791163d7c5e
Alex <alexgilbert909@gmail.com>
2019-09-05 20:13:23 +0100

    First commit

--- src/comps/pages/Done.js
+++ src/comps/pages/Done.js
@@ -1,5 +1,6 @@
 import React, { useContext, useEffect } from 'react';
 import AuthContext from '../../context/auth/authContext';
+import { useSpring, animated } from 'react-spring';
 import tick from '../../img/tick.png';
 
 const Done = props => {
@@ -14,14 +15,16 @@ const Done = props => {
     // eslint-disable-next-line
   }, []);
 
+  let props1 = useSpring({ marginTop: 0, from: { marginTop: -4000 } });
+
   const string =
     'Please verify your email address, you should have recieved an email from us already!';
 
   return (
-    <div className='center-done'>
+    <animated.div style={props1} className='center-done'>
       <img src={tick} alt='' className='tick' />
       <h2>{string}</h2>
-    </div>
+    </animated.div>
   );
 };
 
